b992b981ab13859bf86ca8693fde3c832e753531
nic <xfri3nd@gmail.com>
2022-02-02 19:50:45 -0500

    0213 Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/header/header.jsx'
 import Hero from './components/hero/hero.jsx'
 import MainSection from './components/main-section/main-section.jsx'
 import FeatureCollection from './components/featured-collection/featured-collection.jsx'
+import Footer from './components/footer/footer.jsx'
 import './App.scss';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <MainSection />
       <FeatureCollection />
+      <Footer />
     </div>
   );
 }
